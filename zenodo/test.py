00ae348fce16d654d9472cdae49e0356ec5c7a1e
drifter089 <akshatmittal.0007@gmail.com>
2024-10-01 17:50:59 +0700

    using retry in zenodo

--- zenodo/test.py
+++ zenodo/test.py
@@ -1,5 +1,7 @@
 import zenodopy
 import os
+import time  # Optional: For adding delay between retries
+
 
 version_tag = os.getenv("VERSION_TAG")
 
@@ -7,36 +9,38 @@ print("my msg")
 print(version_tag)
 # zenodo_token = os.getenv("ZENODO_TOKEN")
 
-zeno = zenodopy.Client(
-    sandbox=True, token="CNXNk6GBg0dQUvZJsCUEajG4ZZnAr8YUrWPR9oCucnu8vq39jFJDUGoYW6WK"
-)
+max_retries = 5
 
-# zeno.list_projects
+for attempt in range(1, max_retries + 1):
+    try:
 
-# zeno.create_project(
-#     title="twoversion11",
-#     upload_type="other",
-#     metadata_json="zenodo/.zenodo.json",
-# )
+        zeno = zenodopy.Client(
+            sandbox=True,
+            token="CNXNk6GBg0dQUvZJsCUEajG4ZZnAr8YUrWPR9oCucnu8vq39jFJDUGoYW6WK",
+        )
 
-# print(zeno.deposition_id)
+        zeno.set_project(dep_id=106299)
 
-zeno.set_project(dep_id=106299)
+        zeno.update(
+            source="/home/runner/work/solver-benchmark/solver-benchmark/zenodo/req.txt",
+            publish=True,
+            metadata_json="/home/runner/work/solver-benchmark/solver-benchmark/zenodo/.zenodo.json",
+        )
+        print("Update succeeded.")
+        break  # Exit the loop if the update is successful
+
+    except Exception as e:
+        print(f"Attempt {attempt} failed with error: {e}")
+
+        time.sleep(2)  # Optional: Wait before retrying
+
+        zeno._delete_project(dep_id=106299)
+
+        if attempt == max_retries:
+            print("Max retries reached. Exiting.")
+            raise  # Re-raise the exception to fail after max retries
+        else:
+            time.sleep(2)  # Optional: Wait before retrying
 
 
-# upload file to zenodo
-# zeno.upload_file("zenodo/.zenodo.json", publish=True)
-zeno.update(
-    source="/home/runner/work/solver-benchmark/solver-benchmark",
-    publish=True,
-    metadata_json="zenodo/.zenodo.json",
-)
-# zeno.list_files()
-# zeno.upload_file("test.file.txt",publish=True)
 
-
-# zeno.change_metadata(title="my test file",
-#                         upload_type="text",
-#                         description="Eu reprehenderit non et mollit excepteur nisi laborum labore sit ex ipsum ullamco id. Aute qui fugiat in aute. In quis aute enim cupidatat laborum qui voluptate anim Lorem nulla sint do.",
-#                         creator="akshat")
-# list project id's associated to zenodo account
